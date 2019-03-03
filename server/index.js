852464c954d12747fee49f89ec020b182c7f5aa8
Murendeni Mukwevho <murendeni@Murendenis-MacBook-Pro.local>
2019-03-03 04:06:29 +0200

    Few Changes

--- server/index.js
+++ server/index.js
@@ -3,15 +3,26 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+    const name = req.query.name || 'World';
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+        greeting: `Hello ${name}!`
+    }));
+});
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req, res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
 });
 
 app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+    console.log('Express server is running on localhost:3001')
 );
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { List } from 'react-router-dom';
+
+let time = new Date().toLocaleString();
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="App">
+                Test
+            </div>
+        );
+    }
+}
+export default Home;
